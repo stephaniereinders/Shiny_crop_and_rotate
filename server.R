b3df180b492a3fd3f3b5dc1d35680860c60aba20
Stephanie Reinders <reinders.stephanie@gmail.com>
2022-09-15 09:24:23 -0500

    Display dimensions of scaled image.

--- server.R
+++ server.R
@@ -10,26 +10,37 @@ function(input, output, session) {
   # OBSERVE: image info
   observe({
     image$info <- image_info(image$current)
+    
+    # Original image dimensions
     image$width <- image$info$width
     image$height <- image$info$height
+    
+    # Scaled image width - scale image to fit in window width-wise
+    image$scaled_width = session$clientData$output_image_width
     })
   
   # RENDER: image
   output$image <- renderImage({
     
-    session_width = session$clientData$output_image_width
+    # resize to fit window width-wise. maintain aspect ratio
+    tmp <- image$current %>%
+      magick::image_resize(geometry_size_pixels(width=image$scaled_width)) 
     
-    tmpfile <- image$current %>%
-      magick::image_resize(geometry_size_pixels(width=session_width)) %>%
+    # write to temp file
+    tmpfile <- tmp %>%
       magick::image_write(tempfile(fileext='png'), format = 'png')
     
+    # store scaled height value
+    image$scaled_height <- image_info(tmp)$height
+    
     # Return a list
-    list(src = tmpfile, contentType = "image/png", width=session_width)
+    list(src = tmpfile, contentType = "image/png", width=image$scaled_width)
   }, deleteFile = FALSE)
   
-  # RENDER: original image info
+  # RENDER: image dimensions
   output$width <- renderText({image$width})
   output$height <- renderText({image$height})
-  
+  output$scaled_width <- renderText({image$scaled_width})
+  output$scaled_height <- renderText({image$scaled_height})
   
 }
--- ui.R
+++ ui.R
@@ -9,7 +9,11 @@ fluidPage(
       h5("Original image width:"),
       textOutput("width"),
       h5("Original image height:"),
-      textOutput("height")
+      textOutput("height"),
+      h5("Scaled image width:"),
+      textOutput("scaled_width"),
+      h5("Scaled image height:"),
+      textOutput("scaled_height")
     ),
 
     mainPanel(
