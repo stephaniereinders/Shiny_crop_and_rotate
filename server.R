725a6dcd36de3f2aad8e4a3216ebc45ad4495b74
Stephanie Reinders <reinders.stephanie@gmail.com>
2022-09-15 09:01:27 -0500

    Resize image.
    The app reads and displays the default image, resizing the image to fit the width of the window.

--- server.R
+++ server.R
@@ -1,17 +1,21 @@
 library(magick)
 
 # Define a server for the Shiny app
-function(input, output) {
+function(input, output, session) {
   
   # READ: image
   image <- magick::image_read("sample_writing.png")
   
   # RENDER: image
   output$image <- renderImage({
+    
+    session_width = session$clientData$output_image_width
+    
     tmpfile <- image %>%
+      magick::image_resize(geometry_size_pixels(width=session_width)) %>%
       magick::image_write(tempfile(fileext='png'), format = 'png')
     
     # Return a list
-    list(src = tmpfile, contentType = "image/png")
+    list(src = tmpfile, contentType = "image/png", width=session_width)
   }, deleteFile = FALSE)
 }
